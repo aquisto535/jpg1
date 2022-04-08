eb6bb2e1c028e528871fa49d02a7c5d028e22513
Jung Tae Min <slrkaksemfdj@naver.com>
2022-04-08 21:59:16 +0900

    canvas filling
    canvas filling

--- jpg1-1/project/concept.js
+++ jpg1-1/project/concept.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("jsCanvas")
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName("color")
-
+const mode = document.querySelector(".Filling")
 const INITIAL_COLOR ="#2c2c2c"
 const CANVAS_SIZE = 700
 const range = document.querySelector("input")
@@ -10,9 +10,11 @@ canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE; 
 ctx.strokeStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5
+ctx.fillStyle = INITIAL_COLOR;
 
 
 let painting = false;
+let filing = false
 
 function onMove(event){
     const x = event.offsetX
@@ -39,12 +41,30 @@ function paint(){
 function makeColor(event){
     const color = event.target.style.backgroundColor
     ctx.strokeStyle = color
+    ctx.fillStyle = color;
 }
 
 
 function rangeChange(event){
     const size = event.target.value
     ctx.lineWidth = size;
+}
+
+function handleMode(){
+   if(filing === true){
+    filing = false
+    mode.innerText = "Fill"
+} else{
+    filing = true
+    mode.innerText = "paint"
+    ctx.fillStyle = ctx.strokeStyle
+}
+}
+
+function handleCanvasClick(){
+    if(filing){
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+    }
 }
 
 
@@ -57,7 +77,13 @@ if(canvas){
     canvas.addEventListener("mouseleave" ,onLeave)
     canvas.addEventListener("mousedown", paint)
     canvas.addEventListener("mouseup", onLeave)
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach(color =>
     color.addEventListener("click", makeColor))
+
+mode.addEventListener("click", handleMode)
+    
+
+
